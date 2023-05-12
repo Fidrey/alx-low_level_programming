511f7df6bb6e9fa9715eda01763f8da54892d791
confidence <confidencenwokomah2@gmail.com>
2023-05-11 19:20:38 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,14 +5,15 @@
  *
  * Return: The length of the string.
  */
- int _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	int longit = 0;
 
-	if(*s)
+	if (*s)
 	{
-		longit++
-			longit += _strlen_recursion(s + 1)
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
+
 	return (longit);
 }
