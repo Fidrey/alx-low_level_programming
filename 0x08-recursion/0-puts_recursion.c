82bf08d063a930b920331c8dde176646bb380573
confidence <confidencenwokomah2@gmail.com>
2023-05-11 19:10:01 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,14 @@
  * @s: input
  * Return: always 0(sucesss)
  */
-
-int _puts_recursion(int *s)
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar (*s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
 	else
-		_putchar('\n');
+		putchar('\n');
 }
