4a4070a2b0c4be003af78064168bcbc128592683
confidence <confidencenwokomah2@gmail.com>
2023-05-18 07:01:01 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,16 +2,15 @@
 #include "main.h"
 
 /**
- * main - prints all arguments it receives
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: Always 0 (Success)
- */
+  * main - prints all arguments it receives
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: Always 0 (Success)
+  */
 int main(int argc, char *argv[])
-
 {
 	int c;
+
 	for (c = 0; c < argc; c++)
 	{
 		printf("%s\n", argv[c]);
