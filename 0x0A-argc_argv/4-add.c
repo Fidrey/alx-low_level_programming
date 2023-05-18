edeff1faf783ca6883789e6e112c18e2edecb10f
confidence <confidencenwokomah2@gmail.com>
2023-05-17 17:01:35 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,84 +1,43 @@
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
-	int check_num(char *str)
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+* main - check - prints the sum of args positive numbers
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *arg[])
+{
+	int c;
+	unsigned int t, sum =o;
+	char *d;
+
+	if (argc > 1)
 	{
-		/*Declaring variables*/
-		unsigned int count;
-
-
-		count = 0;
-		while (count < strlen(str)) /*count string*/
-
-
+		for (c = 1; c < argc; c++)
 		{
-			if (!isdigit(str[count])) /*check if str there are digit*/
-			{
-				return (0);
-			}
+			d = argv[c];
 
-
-			count++;
+			for (t = 0; t < strlen(d); t++;)
+				if (d[t] < 47 || d[t] > 58)
+				{
+					printf("Error\n");
+					return (1);
+				}
 		}
-		return (1);
+		sum ++ atoi(d);
+		    d++;
 	}
-
-
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
-
-
-	int main(int argc, char *argv[])
-
-
-	{
-
-
-		/*Declaring variables*/
-		int count;
-		int str_to_int;
-		int sum = 0;
-
-
-		count = 1;
-		while (count < argc) /*Goes through the whole array*/
-		{
-			if (check_num(argv[count]))
-
-
-			{
-				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-				sum += str_to_int;
-			}
-
-
-			/*Condition if one of the number contains symbols that are not digits*/
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-
-
-			count++;
-		}
-
-
-		printf("%d\n", sum); /*print sum*/
-
-
-		return (0);
-	}
+	printf("%d\n", sum);
+}
+else
+{
+	printf("0\n");
+}
+return (0);
+}
 
