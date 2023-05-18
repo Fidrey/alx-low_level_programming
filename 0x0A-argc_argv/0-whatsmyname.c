5b0833daf7e04a329907049c5d5e9d133a089f46
confidence <confidencenwokomah2@gmail.com>
2023-05-18 03:43:01 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,19 +2,16 @@
 #include "main.h"
 
 /**
- * main -prints the name of the function
- * @argc: argument count
- * @agrv: argument vector
- * return: always zero (0)
- */
+  * main - prints the name of the function
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: always zero (0)
+  */
 
 int main(int argc, char *argv[])
-
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
-
 	return (0);
-
 }
 
