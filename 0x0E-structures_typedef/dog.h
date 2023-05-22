b96c1ba253844f0db6ce0c3d5e5c832789796a5e
confidence <confidencenwokomah2@gmail.com>
2023-05-22 15:06:38 -0700

    dog.h header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,14 +3,9 @@
 
 /**
  * struct dog - name of the structure
- * init_dog - Initializes a dog structure.
- * @d: Pointer to the dog structure to initialize.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
- *
- * Description: This function initializes the attributes of a dog structure
- * with the provided name, age, and owner values.
  */
 
 struct dog
@@ -19,10 +14,5 @@ struct dog
 	float age;
 	char *owner;
 };
-
-void init_dog(struct dog *d, char *name, float age, char *owner)
-void print_dog(struct dog *d) /* print_dog - prints a dog structure */
-dog_t *new_dog(char *name, float age, char *owner)
-void free_dog(dog_t *d)
 
 #endif /* DOG_H */
