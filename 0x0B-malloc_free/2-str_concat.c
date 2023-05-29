260574c574e9653dbae95a306d5fc2016576bd1c
confidence <confidencenwokomah2@gmail.com>
2023-05-29 14:35:58 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,50 @@
-#include <stdlib>
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * str_concat : adds two strings of inputs for size
+ * str_concat - adds two strings of inputs for size
  * @s1: input one to concat
  * @s2: input two to concat
  * Description: concatenating two strings s1 and s2
- * return: concat of s1 and s2
+ * Return: concat of s1 and s2
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *s4;
-
-	unsigned int t = 0, j = 0, ten1 = 0, ten2 = 0;
-
-	while (s1 && s1[ten1])
-		ten1++;
-	while (s2 && s2[ten2])
-		ten2++;
-	s4 = malloc(sizeof(char) * (ten1 + ten2 + 1));
-	if (s4 == NULL0
-			return (NULL);
-
-			t = 0;
-			j = 0;
-
-	if (s14)
-	{
-		while (t < ten1)
-			{
-				s4[t] = s1[t];
-					t++;
-				}
-			}
-			if (s2)
-			{ 
-				while (t < (ten1 + ten 2))
-				{
-					s4[t] = s2[j];
-					t++;
-					j++;
-				}
-			}
-			s4[t] = '\0';
-			return (s4);
-}
-
+	int d = 0, e = 0, f = 0, g = 0;
+	char *s;
 
 	if (s1 == NULL)
+		s1 = "";
 
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[d])
+		d++;
+
+	while (s2[e])
+		e++;
+	g = d + e;
+	s = malloc((sizeof(char) * g) + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	e = 0;
+
+	while (f < g)
+	{
+		if (f <= d)
+			s[f] = s1[f];
+		if (f >= d)
+		{
+			s[f] = s2[e];
+			e++;
+		}
+		f++;
+	}
+
+	s[f] = '\0';
+	return (s);
+}
