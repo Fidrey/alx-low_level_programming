4a1586fada8e4a33a3fc1432abe95f88243079e9
confidence <confidencenwokomah2@gmail.com>
2023-05-29 16:35:37 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - converts the parameters to string
+ * @ac: the argunment count
+ * @av: argument vector
+ * Return:always return 0
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int ch = 0, a = 0, b = 0, c = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	while (a < ac)
+	{
+		while (av[a][b])
+		{
+			ch++;
+			b++;
+		}
+
+		b = 0;
+		a++;
+	}
+
+		s = malloc((sizeof(char) * ch) + ac + 1);
+
+		a = 0;
+		while (av[a])
+		{
+			while (av[a][b])
+			{
+				s[c] = av[a][b];
+				c++;
+				b++;
+			}
+
+			s[c] = '\n';
+
+			b = 0;
+			c++;
+			a++;
+		}
+		c++;
+		s[c] = '\0';
+		return (s);
+}
